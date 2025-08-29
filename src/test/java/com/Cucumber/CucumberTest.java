3d76508f3071ce1c6ffe788e5dad2cfaca63ac83
Msdrushi07 <108717120+Msdrushi07@users.noreply.github.com>
2025-08-29 18:27:19 +0530

    Update CucumberTest.java

--- src/test/java/com/Cucumber/CucumberTest.java
+++ src/test/java/com/Cucumber/CucumberTest.java
@@ -13,8 +13,11 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")   // folder under src/test/resources/features
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty",
+                       key = GLUE_PROPERTY_NAME,  value = "com.example.steps")
 @CucumberContextConfiguration
 @SpringBootTest()
 public class CucumberTest {
 }
+
+// if we did not specify the glue property name it will search for step defination inside cucumber tets package or sub package
